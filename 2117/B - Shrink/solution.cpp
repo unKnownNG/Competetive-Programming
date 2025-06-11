e058375b7ee77ebbb4c622e4be7c4829a8a53852
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-06-11 12:54:50 +0530

    Add Shrink [B] from Codeforces

---
+++ 2117/B - Shrink/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+ 
+void solve(){
+    int n;
+    cin >> n;
+    for(int i = 2; i <= n; i++){
+        cout << i << " ";
+    }
+    cout << 1 << endl;
+}
+ 
+int main(){
+    int t;
+    cin >> t;
+    while (t--) solve();
+    
+}
