3f2f6ee3fd4d2f725e283e993c605f9fec0bbc07
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-07-08 13:55:34 +0530

    Add Round Down the Price [A] from Codeforces

---
+++ 1702/A - Round Down the Price/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    // LOGIC
+    long long n, m;
+    cin >> n;
+    m = n;
+    long long c = 1;
+    while(m/10){
+        m /= 10;
+        c *= 10;
+    }
+    cout << abs(n - c) << endl;
+ 
+}
+ 
+int main(){
+    ios::sync_with_stdio(false); // Fast I/O
+    cin.tie(nullptr);            // Untie cin from cout
+ 
+    int n;
+    cin >> n;
+    while(n--) solve();
+}
