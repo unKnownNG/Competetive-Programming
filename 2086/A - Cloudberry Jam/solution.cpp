730fd6e44c100f4000cd949d9dc49717b29a1173
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-07-09 19:22:06 +0530

    Add Cloudberry Jam [A] from Codeforces

---
+++ 2086/A - Cloudberry Jam/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    // LOGIC
+    int n;
+    cin >> n;
+    cout << n * 2 << endl;
+    
+}
+ 
+int main(){
+    ios::sync_with_stdio(false); // Fast I/O
+    cin.tie(nullptr);            // Untie cin from cout
+ 
+    int n;
+    cin >> n;
+    while(n--) solve();
+}
