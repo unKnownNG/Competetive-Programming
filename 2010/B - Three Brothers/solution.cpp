12103dbdce51eab110b6a3cc702e1a767f4376e1
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-07-13 14:26:44 +0530

    Add Three Brothers [B] from Codeforces

---
+++ 2010/B - Three Brothers/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    // LOGIC
+    
+}
+ 
+int main(){
+    ios::sync_with_stdio(false); // Fast I/O
+    cin.tie(nullptr);            // Untie cin from cout
+ 
+    int a, b;
+    cin >> a >> b;
+    cout << abs(6 - (a + b));
+}
