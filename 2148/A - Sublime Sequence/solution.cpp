de81713f24ac27512a4a1caa2934df5f22eb78b3
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-09-14 10:19:30 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; void solve(){    // LOGIC    int n, k;    cin >> n >> k;    if(k&1){        cout << n ;    }    else{        cout << 0;    }    cout << endl;} int main(){    ios::sync_with_stdio(false); // Fast I/O    cin.tie(nullptr);            // Untie cin from cout     int n;    cin >> n;    while(n--) solve();}
