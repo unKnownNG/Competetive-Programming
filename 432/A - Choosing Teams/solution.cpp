7ff441299188c9e6ad877ed415797f43885f0c27
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-10-05 22:03:39 +0530

    Add Choosing Teams [A] from Codeforces

---
+++ 432/A - Choosing Teams/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; void solve(){    // LOGIC    int n, k;    cin >> n >>k;     int c = 0;    for(int i = 0; i < n; i++){        int a;        cin >> a;        if(a <= (5 - k)) c++;    }     cout << c / 3;} int main(){    ios::sync_with_stdio(false); // Fast I/O    cin.tie(nullptr);            // Untie cin from cout  solve();}
