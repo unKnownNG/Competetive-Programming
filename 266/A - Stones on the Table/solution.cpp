64c15251becd0528ce797066874c3d79dd8150a9
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-10-23 21:30:33 +0530

    Add Stones on the Table [A] from Codeforces

---
+++ 266/A - Stones on the Table/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; void solve(){    // LOGIC    int n;    cin >> n;     string s;    cin >> s;    int c = 0;     for(int i = 0; i< n - 1; i++){        if(s[i] == s[i + 1]) c++;    }      cout << c << endl;} int main(){    ios::sync_with_stdio(false); // Fast I/O    cin.tie(nullptr);            // Untie cin from cout      solve();}
