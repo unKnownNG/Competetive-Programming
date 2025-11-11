bcd68095f8cfa960c652bcae7298dbd9746b4bae
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2025-11-11 21:22:03 +0530

    Add Fancy Fence [A] from Codeforces

---
+++ 270/A - Fancy Fence/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; void solve(){    int a;    cin >> a;        // Check if a valid regular polygon exists    if (360 % (180 - a) == 0)        cout << "YES\n";    else        cout << "NO\n";} int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);     int t;    cin >> t;    while(t--) solve();}
