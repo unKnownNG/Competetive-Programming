936760ff1672df02d4a12097a12086be584ae35d
Mohammed Daiyaan <91405849+unKnownNG@users.noreply.github.com>
2026-08-13 23:01:17 +0530

    Add Tea Tasting [C] from Codeforces

---
+++ 1795/C - Tea Tasting/solution.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "
+"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+ 
+template<typename T> using v = vector<T>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+ 
+template<typename T1, typename T2> using pr = pair<T1, T2>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+ 
+ 
+ 
+void solve(){
+    // Logic
+    ll n;
+    cin >> n;
+    vll v(n), t(n);
+ 
+    for(auto &i : v) cin >> i;
+    for(auto &i : t) cin >> i;
+ 
+    vll pref(n + 1), diff(n + 1), rem(n + 1);
+ 
+ 
+    for(int i = 0; i < n; i++){
+        pref[i + 1] = t[i] + pref[i];
+    }
+ 
+ 
+    for(int i = 0; i < n; i++){
+        ll tar = v[i] + pref[i ];
+ 
+        ll idx = upper_bound(pref.begin() + i + 1, pref.begin() + n + 1, tar) - pref.begin();
+ 
+ 
+        diff[i] += 1;
+        diff[idx - 1] -= 1;
+        if(idx - 1 < n )rem[idx - 1] += v[i] -pref[idx- 1] +  pref[i ];
+ 
+ 
+    }
+ 
+ 
+    ll cur = 0;
+ 
+    for(int i = 0; i < n; i++){
+        cur += diff[i];
+ 
+        ll c = (cur * t[i]) + rem[i];
+ 
+        cout << c <<  " ";
+    }
+ 
+    cout << endl;
+}
+ 
+ 
+int main() {
+#ifndef ONLINE_JUDGE
+freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--) solve();
+ 
+}
